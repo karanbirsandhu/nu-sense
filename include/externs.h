3c211617bb127be05757768d42c691ef6c132463
karanbirsandhu <56524111+karanbirsandhu@users.noreply.github.com>
2019-12-16 23:42:02 -0500

    extern  variable
    update variable names and values to access them globally

---
+++ include/externs.h
@@ -0,0 +1,7 @@
+
+extern int sensor_number; // Number of sensors
+extern int data_number; // Number of datas
+extern int group_number; // Number of groups
+extern int temperatureMin; // Minimum value of temperature
+extern int temperatureMax; // Maximum value of temperature
+
